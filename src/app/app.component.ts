f74892463f1509fd0c4ba238bec5843cb8d77f4c
Srini Raju Indukuri <srini.raju@ultria.com>
2021-10-13 15:39:12 +0530

    Changes made

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'SmallestNumber';
+  output = '';
+
+  findSmall(input: string) {
+    try {
+      var numberStringArray = input.split(',');
+      var numberArray = numberStringArray.map((v) => +v);
+      var result = numberArray.reduce(
+        (p, c) => (p < c ? p : c),
+        numberArray[0]
+      );
+      this.output = 'The minumum number is ' + result;
+    } catch (error) {
+      this.output = 'Error while procsessing';
+    }
+  }
 }
